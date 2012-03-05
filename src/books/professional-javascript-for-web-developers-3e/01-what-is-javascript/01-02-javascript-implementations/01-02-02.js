98b5b9b438a10da1a421ef816981e3e1141a5ff7
luotao <luotaoyeah@gmail.com>
2012-03-05 18:52:46 +0800

    professional-javascript-for-web-developers: 01-02-02: BOM

---
+++ src/books/professional-javascript-for-web-developers-3e/01-what-is-javascript/01-02-javascript-implementations/01-02-02.js
@@ -0,0 +1,20 @@
+/*
+ * 1. What Is JavaScript
+ *      1.2. JavaScript Implementations
+ *          1.2.2. BOM
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * BOM（Browser Object Model）（浏览器对象模型）直到 HTML5 才有了标准，
+   * 简单来讲，它主要指的是 window 对象，
+   * 而 DOM（Document Object Model）（文档对象模型）主要指的是 window.document 对象，
+   * BOM 主要包括下面一些对象和属性：
+   *     window.navigator
+   *     window.location
+   *     window.screen
+   *     window.history
+   *     window.XMLHttpRequest
+   */
+}
