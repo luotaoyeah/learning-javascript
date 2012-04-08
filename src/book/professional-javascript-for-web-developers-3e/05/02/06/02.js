5e2f1a7860a1351d1445ae88645deaf7296a3b5d
luotao <luotaoyeah@gmail.com>
2012-04-08 10:09:15 +0800

    feat(professional-javascript-for-web-developers): Array.slice()

---
+++ src/book/professional-javascript-for-web-developers-3e/05/02/06/02.js
@@ -0,0 +1,28 @@
+// manipulation-methods
+
+'use strict';
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // `Array.slice()` 方法, 截取数组的一部分, 返回新的数组,
+  // 可以指定开始索引和结束索引, 默认为 [0,length),
+  // 结束索引位置的元素不会被包括
+
+  var arr01 = [1, 2, 3, 4, 5];
+  console.assert(JSON.stringify(arr01.slice()) === JSON.stringify([1, 2, 3, 4, 5]));
+  console.assert(JSON.stringify(arr01.slice(1)) === JSON.stringify([2, 3, 4, 5]));
+  console.assert(JSON.stringify(arr01.slice(undefined, 2)) === JSON.stringify([1, 2]));
+  console.assert(JSON.stringify(arr01.slice(1, 2)) === JSON.stringify([2]));
+
+  // 如果结束索引 >= 开始索引, 则返回空数组
+  console.assert(JSON.stringify(arr01.slice(1, 1)) === JSON.stringify([]));
+})();
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // 如果索引为负数, 则索引会被取模变成正数
+
+  var arr01 = [1, 2, 3, 4, 5];
+
+  console.assert(JSON.stringify(arr01.slice(-4, -2)) === JSON.stringify(arr01.slice(1, 3)));
+})();
