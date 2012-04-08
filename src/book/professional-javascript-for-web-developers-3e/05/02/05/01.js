3413c7e57bfe4ae97f330207aa2a00b63908d59e
luotao <luotaoyeah@gmail.com>
2012-04-08 09:40:20 +0800

    feat(professional-javascript-for-web-developers): Array.concat()

--- src/book/professional-javascript-for-web-developers-3e/05/02/05/01.js
+++ src/book/professional-javascript-for-web-developers-3e/05/02/05/01.js
@@ -4,21 +4,10 @@
 
 (function () {
   // ----------------------------------------------------------------------------------------------------
-  // `Array.concat()` 方法,
-  // 将参数添加到数组末尾, 返回新的数组
+  // `Array.reverse()` 将数组倒序,
+  // 直接修改数据, 并且返回数组
 
-  var arr01 = [1, 2];
-  var arr02 = arr01.concat(3, 4);
-
-  console.assert(JSON.stringify(arr01) === JSON.stringify([1, 2]));
-  console.assert(JSON.stringify(arr02) === JSON.stringify([1, 2, 3, 4]));
-})();
-
-(function () {
-  // ----------------------------------------------------------------------------------------------------
-  // 如果参数是一个 array, 则它会被平化一层
-
-  var arr01 = [1, 2];
-  var arr02 = arr01.concat(3, [4, [5, [6]]], [7]);
-  console.assert(JSON.stringify(arr02) === JSON.stringify([1, 2, 3, 4, [5, [6]], 7]));
+  var arr = [1, 2, 3];
+  arr.reverse();
+  console.assert(JSON.stringify(arr) === JSON.stringify([3, 2, 1]));
 })();
---
+++ src/book/professional-javascript-for-web-developers-3e/05/02/06/01.js
@@ -0,0 +1,24 @@
+// manipulation-methods
+
+'use strict';
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // `Array.concat()` 方法,
+  // 将参数添加到数组末尾, 返回新的数组
+
+  var arr01 = [1, 2];
+  var arr02 = arr01.concat(3, 4);
+
+  console.assert(JSON.stringify(arr01) === JSON.stringify([1, 2]));
+  console.assert(JSON.stringify(arr02) === JSON.stringify([1, 2, 3, 4]));
+})();
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // 如果参数是一个 array, 则它会被平化一层
+
+  var arr01 = [1, 2];
+  var arr02 = arr01.concat(3, [4, [5, [6]]], [7]);
+  console.assert(JSON.stringify(arr02) === JSON.stringify([1, 2, 3, 4, [5, [6]], 7]));
+})();
