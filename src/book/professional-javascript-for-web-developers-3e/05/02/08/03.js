f5a1c5a4e5bcc6ae34fa25510a6ba27ec6dd82ae
luotao <luotaoyeah@gmail.com>
2012-04-10 17:32:51 +0800

    feat(professional-javascript-for-web-developers): Array.filter()

---
+++ src/book/professional-javascript-for-web-developers-3e/05/02/08/03.js
@@ -0,0 +1,18 @@
+// iterative-methods
+
+'use strict';
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // `Array.filter()` 方法,
+  // 返回满足条件的元素组成的新数组
+
+  var arr = [1, 2, 3, 4, 5];
+  console.assert(
+    JSON.stringify(
+      arr.filter(function (item) {
+        return item % 2 === 0;
+      }),
+    ) === JSON.stringify([2, 4]),
+  );
+})();
