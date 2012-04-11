60c05e870fee3bcae0f27c230d46b55b6f6b848f
luotao <luotaoyeah@gmail.com>
2012-04-11 10:01:56 +0800

    feat(professional-javascript-for-web-developers): Array.reduceRight()

---
+++ src/book/professional-javascript-for-web-developers-3e/05/02/09/02.js
@@ -0,0 +1,20 @@
+// reduction-methods
+
+'use strict';
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // `Array.reduceRight()` 方法,
+  // 跟 `Array.reduce()` 的区别: 从后往前遍历
+
+  var intermediateResults = [];
+
+  console.assert(
+    [1, 2, 3, 4, 5].reduceRight(function (prev, item, index, array) {
+      intermediateResults.push(prev);
+      return prev + item;
+    }, 0) === 15,
+  );
+
+  console.assert(JSON.stringify(intermediateResults) === JSON.stringify([0, 5, 9, 12, 14]));
+})();
