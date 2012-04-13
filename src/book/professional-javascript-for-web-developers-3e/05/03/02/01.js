6e733c070160d4f6e8e8c2174b499c0f6cd814de
luotao <luotaoyeah@gmail.com>
2012-04-13 09:44:30 +0800

    feat(professional-javascript-for-web-developers): Date.toISOString()

---
+++ src/book/professional-javascript-for-web-developers-3e/05/03/02/01.js
@@ -0,0 +1,20 @@
+// date-formatting-methods
+
+'use strict';
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // 几个用于格式化的方法, 不同浏览器有不同实现
+
+  var date = new Date('2012-04-01T13:01:01.000+0800');
+
+  console.assert(date.toDateString() === 'Sun Apr 01 2012');
+  console.assert(date.toTimeString() === '13:01:01 GMT+0800 (GMT+08:00)');
+
+  console.assert(date.toLocaleDateString() === '2012-4-1');
+  console.assert(date.toLocaleTimeString() === '13:01:01');
+
+  console.assert(date.toUTCString() === 'Sun, 01 Apr 2012 05:01:01 GMT');
+
+  console.assert(date.toISOString() === '2012-04-01T05:01:01.000Z');
+})();
