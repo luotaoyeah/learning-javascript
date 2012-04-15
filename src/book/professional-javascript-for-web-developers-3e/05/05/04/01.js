e79efe55496e07b1e95a6d32f94d56c138387868
luotao <luotaoyeah@gmail.com>
2012-04-15 11:00:58 +0800

    feat(professional-javascript-for-web-developers): arguments.callee

---
+++ src/book/professional-javascript-for-web-developers-3e/05/05/04/01.js
@@ -0,0 +1,17 @@
+// functions-as-values
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // NON-STRICT 模式下,
+  // `arguments.callee` 表示当前函数对象本身
+
+  function fn01() {
+    console.assert(arguments.callee instanceof Function);
+    return arguments.callee.x;
+  }
+  console.assert(fn01() === undefined);
+
+  var fn02 = fn01;
+  fn02.x = 1;
+  console.assert(fn02() === 1);
+})();
