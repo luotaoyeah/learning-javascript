603fc004c79f87edb21f4c9ce02d4731e47adda0
luotao <luotaoyeah@gmail.com>
2012-04-16 16:12:20 +0800

    feat(professional-javascript-for-web-developers): Function.prototype.length

---
+++ src/book/professional-javascript-for-web-developers-3e/05/05/05/01.js
@@ -0,0 +1,10 @@
+// function-properties-and-methods
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // `Function.prototype.length` 属性, 表示形参个数
+
+  console.assert(function () {}.length === 0);
+  console.assert(function (x) {}.length === 1);
+  console.assert(function (x, y) {}.length === 2);
+})();
