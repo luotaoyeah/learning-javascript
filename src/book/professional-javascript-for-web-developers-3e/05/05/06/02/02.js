0a4747e3de364b22c58f84517b4a14b3daeaeebd
luotao <luotaoyeah@gmail.com>
2012-04-17 10:31:44 +0800

    feat(professional-javascript-for-web-developers): Number.prototype.toString()

---
+++ src/book/professional-javascript-for-web-developers-3e/05/05/06/02/02.js
@@ -0,0 +1,12 @@
+// primitive-wrapper-types
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // `Number.prototype.toString()` 方法, 可以指定进制
+
+  var num01 = new Number(10);
+  console.assert(num01.toString() === '10');
+  console.assert(num01.toString(2) === '1010');
+  console.assert(num01.toString(8) === '12');
+  console.assert(num01.toString(16) === 'a');
+})();
