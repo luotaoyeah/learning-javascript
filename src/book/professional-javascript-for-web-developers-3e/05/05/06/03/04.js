af09e096fc1e607c38de98d6be63d37a20a9d00d
luotao <luotaoyeah@gmail.com>
2012-04-18 13:13:23 +0800

    feat(professional-javascript-for-web-developers): String.prototype.indexOf()

---
+++ src/book/professional-javascript-for-web-developers-3e/05/05/06/03/04.js
@@ -0,0 +1,34 @@
+// string-manipulation-methods
+
+'use strict';
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // `String.prototype.indexOf()` 查找子串所在的索引, 找不到则返回 -1
+
+  var str = 'hello world';
+  console.assert(str.indexOf('o') === 4);
+  console.assert(str.indexOf('x') === -1);
+})();
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // `String.prototype.lastInexOf()` 从后往前查找
+
+  var str = 'hello world';
+  console.assert(str.indexOf('o') === 4);
+  console.assert(str.lastIndexOf('o') === 7);
+})();
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // 可以指定开始查找的索引
+
+  var str = 'hello world';
+
+  console.assert(str.indexOf('o') === 4);
+  console.assert(str.indexOf('o', 5) === 7);
+
+  console.assert(str.lastIndexOf('o') === 7);
+  console.assert(str.lastIndexOf('o', 6) === 4);
+})();
