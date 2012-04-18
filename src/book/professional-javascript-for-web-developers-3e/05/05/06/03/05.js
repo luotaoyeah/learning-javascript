3a58d35bb12414dd03a60d7e3d902101c7b7b9c0
luotao <luotaoyeah@gmail.com>
2012-04-18 13:21:13 +0800

    feat(professional-javascript-for-web-developers): String.prototype.trim()

---
+++ src/book/professional-javascript-for-web-developers-3e/05/05/06/03/05.js
@@ -0,0 +1,18 @@
+// string-manipulation-methods
+
+'use strict';
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+
+  var str = ' \t\v\fhello world\r\n ';
+
+  // `String.prototype.trim()` 去除首位空格
+  console.assert(str.trim() === 'hello world');
+
+  // `String.prototype.trimLeft()` 去除头部空格
+  console.assert(str.trimLeft() === 'hello world\r\n ');
+
+  // `String.prototype.trimRight()` 去除尾部空格
+  console.assert(str.trimRight() === ' \t\v\fhello world');
+})();
