31d7babb8464c739260eebc423a3e83682af4c0e
luotao <luotaoyeah@gmail.com>
2012-04-21 11:39:59 +0800

    feat(professional-javascript-for-web-developers): Math.random()

---
+++ src/book/professional-javascript-for-web-developers-3e/05/05/07/02/04.js
@@ -0,0 +1,40 @@
+// the-random-methods
+
+'use strict';
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // `Math.random()` 返回 (0,1) 之间的伪随机数
+
+  for (var i = 0; i < 1000; i++) {
+    console.assert(Math.random() > 0);
+    console.assert(Math.random() < 1);
+  }
+})();
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+
+  /**
+   * 返回指定范围中的整数
+   * @param min 最小值
+   * @param max 最大值
+   * @returns {number}
+   */
+  function fromRange(min, max) {
+    if (max < min) {
+      throw new Error("max can't be less than min");
+    }
+
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  for (var i = 0; i < 1000; i++) {
+    console.assert(fromRange(3, 6) >= 3);
+    console.assert(fromRange(3, 6) <= 6);
+  }
+
+  for (var i = 0; i < 1000; i++) {
+    console.assert(fromRange(5, 5) === 5);
+  }
+})();
