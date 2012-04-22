b8402ce28dfd3eee477894014deb79f7bd627796
luotao <luotaoyeah@gmail.com>
2012-04-22 17:26:40 +0800

    feat(professional-javascript-for-web-developers): Object.defineProperties()

---
+++ src/book/professional-javascript-for-web-developers/06/01/03.js
@@ -0,0 +1,24 @@
+// defining-multiple-properties
+
+'use strict';
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // `Object.defineProperties()` 方法, 同时定义多个属性
+
+  var obj = {};
+
+  Object.defineProperties(obj, {
+    x: {
+      value: 1,
+    },
+    y: {
+      get: function () {
+        return 2;
+      },
+    },
+  });
+
+  console.assert(obj.x === 1);
+  console.assert(obj.y === 2);
+})();
