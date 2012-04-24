503267ee9b994362157aad8080602144747374ff
luotao <luotaoyeah@gmail.com>
2012-04-24 13:51:59 +0800

    feat(professional-javascript-for-web-developers): native-object-prototypes

---
+++ src/book/professional-javascript-for-web-developers/06/02/10.js
@@ -0,0 +1,26 @@
+// native-object-prototypes
+
+'use strict';
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // 内置类型的 prototype 对象可以被修改
+
+  /**
+   * 判断一个属性是否是一个 prototype 属性
+   * @param key
+   * @returns {boolean}
+   */
+  Object.prototype.hasPrototypeProperty = function (key) {
+    return !this.hasOwnProperty(key) && key in this;
+  };
+
+  function Person(a) {
+    this.a = a;
+  }
+
+  Person.prototype.b = 'b';
+
+  console.assert(new Person().hasOwnProperty('a'));
+  console.assert(new Person().hasPrototypeProperty('b'));
+})();
