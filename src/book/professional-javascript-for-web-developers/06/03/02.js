8f712e62db15fb2125be26d8d94a31034fe3d479
luotao <luotaoyeah@gmail.com>
2012-04-26 10:13:59 +0800

    feat(professional-javascript-for-web-developers): default-prototypes

---
+++ src/book/professional-javascript-for-web-developers/06/03/02.js
@@ -0,0 +1,10 @@
+// default-prototypes
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // prototype chain 的最顶端一定是 `Object.prototype` 对象
+
+  function Person() {}
+
+  console.assert(Object.getPrototypeOf(Person.prototype) === Object.prototype);
+})();
