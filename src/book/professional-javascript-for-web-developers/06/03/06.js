f22a933e6bdf17bc4497b1f187a41e90eaedb96c
luotao <luotaoyeah@gmail.com>
2012-04-27 11:19:46 +0800

    feat(professional-javascript-for-web-developers): parasitic-combination-inheritance

--- src/book/professional-javascript-for-web-developers/06/03/06.js
+++ src/book/professional-javascript-for-web-developers/06/03/06.js
@@ -5,7 +5,7 @@
   // 同时使用 constructor stealing 和 prototype chain
 
   function Parent(x) {
-    this.x = x;
+    this.x = x || 0;
     this.items = ['a', 'b', 'c'];
   }
 
@@ -30,4 +30,44 @@
   console.assert(child01.items !== child02.items);
   console.assert(child01.walk === child02.walk);
   console.assert(child01.talk === child02.talk);
+
+  console.assert(child01.x === 1);
+  delete child01.x;
+  console.assert(child01.x === 0);
+})();
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // 上面的实现有一个问题: 父类的 instance property 和子类的 instance property 重复定义了,
+  // 可以如下解决
+
+  function Parent(x) {
+    this.x = x;
+  }
+
+  Parent.prototype.walk = function () {
+    return 'walking';
+  };
+
+  function Child(x, y) {
+    Parent.call(this, x);
+
+    this.y = y;
+  }
+
+  Child.prototype = Object.create(Parent.prototype);
+
+  Child.prototype.talk = function () {
+    return 'talking';
+  };
+
+  var child01 = new Child(1, 2);
+  var child02 = new Child(3, 4);
+
+  console.assert(child01.walk === child02.walk);
+  console.assert(child01.talk === child02.talk);
+
+  console.assert(child01.x === 1);
+  delete child01.x;
+  console.assert(child01.x === undefined);
 })();
