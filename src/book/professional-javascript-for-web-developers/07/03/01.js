474bff30f46ce275846954ffb686d8f5b7c7335b
luotao <luotaoyeah@gmail.com>
2012-04-29 10:04:57 +0800

    feat(professional-javascript-for-web-developers): mimicking-block-scope

---
+++ src/book/professional-javascript-for-web-developers/07/03/01.js
@@ -0,0 +1,26 @@
+// mimicking-block-scope
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+  // javascript 只有 global scope 和 function scope, 没有 block scope,
+  // 可以使用 IIFE(Immediately Invoked Function Expression) 来模拟 block scope
+
+  for (var i = 0; i < 10; i++) {}
+
+  console.assert(i === 10);
+})();
+
+(function () {
+  // ----------------------------------------------------------------------------------------------------
+
+  (function () {
+    for (var i = 0; i < 10; i++) {}
+  })();
+
+  try {
+    console.assert(i === undefined);
+  } catch (e) {
+    console.assert(e instanceof ReferenceError);
+    console.assert(e.message === 'i is not defined');
+  }
+})();
