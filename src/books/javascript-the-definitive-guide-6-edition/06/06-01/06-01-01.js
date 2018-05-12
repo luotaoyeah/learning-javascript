dc5aab5e6f80f77a81e12de4719e55a1fe39ac82
luotao <luotaoyeah@gmail.com>
2018-05-12 19:15:38 +0800

    the definitive guide: object literals

---
+++ src/books/javascript-the-definitive-guide-6-edition/06/06-01/06-01-01.js
@@ -0,0 +1,27 @@
+console.log("\n-------------------------------------------------- 01");
+/* 空对象，没有任何属性 */
+var empty = {};
+var point = {
+  x: 0,
+  y: 0
+};
+/* 属性值是表达式 */
+var point02 = {
+  x: point.x + 1,
+  y: point.y + 1
+};
+var book = {
+  /* 属性名包含空格，使用引号包裹起来 */
+  "main title": "JavaScript",
+  /* 属性名包含连接符号，使用引号包裹起来 */
+  "sub-title": "The definitive Guide",
+  /* 属性名是关键字 */
+  for: "developers",
+  /* 属性值是一个对象 */
+  author: {
+    firstname: "David",
+    surname: "Flanagan"
+  }
+};
+
+console.log(JSON.stringify(book, null, 4));
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6-edition/05-statements/05-07-misc-statements/05-07-03-use-strict/index");
+require("./books/javascript-the-definitive-guide-6-edition/06/06-01/06-01-01");
