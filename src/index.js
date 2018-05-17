83d6564f703f4a6f756cdfd17755e5680a148102
luotao <luotaoyeah@gmail.com>
2018-05-17 09:36:39 +0800

    Array.prototype.concat: example: concatenating three arrays

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./mdn/reference/global-objects/array/concat/concat-02");
+require("./mdn/reference/global-objects/array/concat/concat-03");
---
+++ src/mdn/reference/global-objects/array/concat/concat-03.js
@@ -0,0 +1,9 @@
+/*
+ * Example: Concatenating three arrays
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/* [ 1, 2, 3, 4, 5 ] */
+console.log([1, 2, 3].concat([4], [5]));
+
+export {};
