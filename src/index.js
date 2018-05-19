546a92457b0827e4551a1ac4b285c7bcda1d6514
luotao <luotaoyeah@gmail.com>
2018-05-19 08:11:56 +0800

    the definitive guide: 6.1.4 Object.create(): example 6-1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6-edition/06/06-01/06-01-04/06-01-04-C");
+require("./books/javascript-the-definitive-guide-6-edition/06/06-01/06-01-04/example-6-1");
