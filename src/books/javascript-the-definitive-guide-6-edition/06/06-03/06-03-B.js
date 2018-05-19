13f890ca5f3238382630b30eae61b34b66707336
luotao <luotaoyeah@gmail.com>
2018-05-19 13:31:08 +0800

    the definitive guide: 6.3 deleting properties: delete own properties, not inherited properties

---
+++ src/books/javascript-the-definitive-guide-6-edition/06/06-03/06-03-B.js
@@ -0,0 +1,23 @@
+/*
+ * delete 只会删除 own properties，不会删除 inherited properties；
+ */
+import { inherit } from "../06-01/06-01-04/example-6-1";
+
+console.log("\n-------------------------------------------------- 01");
+
+const obj01 = { x: 1 };
+const obj02 = inherit(obj01);
+obj02.y = 2;
+
+/* { y: 2 } */
+console.log(obj02);
+
+delete obj02.x;
+delete obj02.y;
+
+/* {} */
+console.log(obj02);
+/* { x: 1 } */
+console.log(obj01);
+
+export {};
