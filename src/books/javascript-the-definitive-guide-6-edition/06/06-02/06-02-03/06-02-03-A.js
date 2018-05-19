2b6aa439e076b51e29eab5ab94931a2ffce933c9
luotao <luotaoyeah@gmail.com>
2018-05-19 12:32:28 +0800

    the definitive guide: 6.2.3 property access errors

---
+++ src/books/javascript-the-definitive-guide-6-edition/06/06-02/06-02-03/06-02-03-A.js
@@ -0,0 +1,14 @@
+/*
+ * 6.2.3 Property Access Errors
+ */
+
+/*
+ * 访问对象上不存在的属性（own property 或者 inherited property），返回 undefined；
+ */
+console.log("\n-------------------------------------------------- 01");
+
+const obj = {};
+/* undefined */
+console.log(obj.x);
+
+export {};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6-edition/06/06-02/06-02-02/06-02-02-C");
+require("./books/javascript-the-definitive-guide-6-edition/06/06-02/06-02-03/06-02-03-A");
