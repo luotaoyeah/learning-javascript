5519c651b4d66122c7c3009cba6d4d86830ede46
luotao <luotaoyeah@gmail.com>
2018-05-19 08:02:15 +0800

    the definitive guide: 6.1.4 Object.create(): empty object

---
+++ src/books/javascript-the-definitive-guide-6-edition/06/06-01/06-01-04/06-01-04-C.js
@@ -0,0 +1,16 @@
+/*
+ * 如果想创建一个空对象，类似 {} 或者 new Object()，参数应该指定为 Object.prototype；
+ */
+console.log("\n-------------------------------------------------- 01");
+
+const obj01 = {};
+const obj02 = new Object();
+const obj03 = Object.create(Object.prototype);
+/* true */
+console.log(obj01.__proto__ === Object.prototype);
+/* true */
+console.log(obj01.__proto__ === obj02.__proto__);
+/* true */
+console.log(obj01.__proto__ === obj03.__proto__);
+
+export {};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./mdn/reference/global-objects/object/create/create-00");
+require("./books/javascript-the-definitive-guide-6-edition/06/06-01/06-01-04/06-01-04-C");
