880891c3fb041e56c96556ed526e96ae8992d4b0
luotao <luotaoyeah@gmail.com>
2018-05-20 13:38:20 +0800

    Object.create(): with the second parameter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6-edition/06/06-07/06-07-F");
+require("./mdn/reference/global-objects/object/create/create-01");
---
+++ src/mdn/reference/global-objects/object/create/create-01.js
@@ -0,0 +1,22 @@
+/*
+ * 可以指定第二个参数，表示 property descriptors，类似 Object.defineProperties() 的第二个参数；
+ */
+console.log("\n-------------------------------------------------- 01");
+
+const obj01 = Object.create(
+  { x: 1 },
+  {
+    y: {
+      value: 2
+    },
+    sum: {
+      get() {
+        return this.x + this.y;
+      }
+    }
+  }
+);
+/* 3 */
+console.log(obj01.sum);
+
+export {};
