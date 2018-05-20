b81c910badf917638221658a34d8d9e4de29e15e
luotao <luotaoyeah@gmail.com>
2018-05-20 15:02:47 +0800

    the definitive guide: 6.7 property attributes: cannot change a non-configurable data property to an accessor property

---
+++ src/books/javascript-the-definitive-guide-6-edition/06/06-07/06-07-G/06-07-G-04.js
@@ -0,0 +1,26 @@
+/*
+ * 如果一个 data property 是 non-configurable 的，则不能将它修改为 accessor property；
+ */
+console.log("\n-------------------------------------------------- 01");
+
+const obj01 = Object.create(
+  {},
+  {
+    x: {
+      value: 1,
+      writable: true,
+      enumerable: true
+    }
+  }
+);
+
+/*
+  /!* TypeError: Cannot redefine property: x *!/
+  Object.defineProperty(obj01, "x", {
+    get() {
+      return 0;
+    }
+  });
+*/
+
+export {};
