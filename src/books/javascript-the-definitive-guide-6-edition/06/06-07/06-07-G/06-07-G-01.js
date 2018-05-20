7e4e32e436cfa4c9136022e4654e9b5ee3f98b3d
luotao <luotaoyeah@gmail.com>
2018-05-20 13:47:04 +0800

    the definitive guide: 6.7 property attributes: cannot add property on non-extensible object

---
+++ src/books/javascript-the-definitive-guide-6-edition/06/06-07/06-07-G/06-07-G-01.js
@@ -0,0 +1,23 @@
+/*
+ * Object.defineProperty() 和 Object.defineProperties() 在下面几种情况下会报错；
+ */
+
+/*
+ * 如果一个对象是 non-extensible 的，则不能给对象添加新的属性；
+ */
+console.log("\n-------------------------------------------------- 01");
+
+const obj01 = { x: 1 };
+Object.preventExtensions(obj01);
+
+/*
+  /!* TypeError: Cannot add property y, object is not extensible *!/
+  obj01.y = 2;
+*/
+
+/*
+  /!* TypeError: Cannot define property y, object is not extensible *!/
+  Object.defineProperty(obj01, "y", { value: 2 });
+*/
+
+export {};
