c28ba15c9a325d76e30df74f944b4b7ec427e5b9
luotao <luotaoyeah@gmail.com>
2018-05-21 21:43:37 +0800

    the definitive guide: 6.8 object attributes: Object.prototype.isPrototypeOf()

---
+++ src/books/javascript-the-definitive-guide-6-edition/06/06-08/06-08-01/06-08-01-C.js
@@ -0,0 +1,32 @@
+/*
+ * Object.prototype.isPrototypeOf()：
+ *    判断一个对象是否在另一个对象的 prototype chain 上，
+ *    即一个对象是否是另一个对象（或其 prototype，其 prototype 的 prototype，ETC）的 prototype；
+ */
+console.log("\n-------------------------------------------------- 01");
+
+const obj01 = {};
+const obj02 = Object.create(obj01);
+
+/* true */
+console.log(obj01.isPrototypeOf(obj02));
+/* true */
+console.log(Object.prototype.isPrototypeOf(obj02));
+
+/*
+ * instanceof：
+ *     可以用来判断一个对象是否是一个构造函数的实例；
+ */
+console.log("\n-------------------------------------------------- 01");
+
+const obj03 = {};
+const obj04 = Object.create(obj03);
+
+/* true */
+console.log(obj03.isPrototypeOf(obj04));
+/* true */
+console.log(obj04 instanceof obj03.constructor);
+/* true */
+console.log(obj04 instanceof Object.prototype.constructor);
+
+export {};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6-edition/06/06-08/06-08-01/06-08-01-B");
+require("./books/javascript-the-definitive-guide-6-edition/06/06-08/06-08-01/06-08-01-C");
