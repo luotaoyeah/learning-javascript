086f48918b63f237127bb387f3b3db101ecd2c49
luotao <luotaoyeah@gmail.com>
2018-05-23 21:40:32 +0800

    the definitive guide: 6.8.3: Object.isFrozen()

---
+++ src/books/javascript-the-definitive-guide-6-edition/06/06-08/06-08-03/06-08-03-F.js
@@ -0,0 +1,29 @@
+/*
+ * Object.isFrozen()：
+ *     判断对象是否处于 frozen 状态；
+ */
+console.log("\n-------------------------------------------------- 01");
+const obj01 = {};
+/* false */
+console.log(Object.isFrozen(obj01));
+Object.freeze(obj01);
+/* true */
+console.log(Object.isFrozen(obj01));
+
+/*
+ * 可以手动设置对象的特性，使对象处于 frozen 状态；
+ */
+console.log("\n-------------------------------------------------- 01");
+const obj02 = { x: "x" };
+/* false */
+console.log(Object.isFrozen(obj02));
+
+Object.preventExtensions(obj02);
+Object.defineProperty(obj02, "x", {
+  configurable: false,
+  writable: false
+});
+/* true */
+console.log(Object.isFrozen(obj02));
+
+export {};
