cb6c8ac3785cae10fc503e4987802663b1372b48
luotao <luotaoyeah@gmail.com>
2018-05-25 21:30:36 +0800

    the definitive guide: 7.1: array literal

--- src/books/javascript-the-definitive-guide-6e/07/07-01/07-01-A.js
+++ src/books/javascript-the-definitive-guide-6e/07/07-01/07-01-A.js
@@ -7,9 +7,31 @@
  *     元素由 comma 分隔，包含在 square brackets 中；
  */
 console.log("\n-------------------------------------------------- 01");
-
 const array01 = [];
 const array02 = [1, 2, 3];
 const array03 = [0, false, "x", null];
+
+/*
+ * array literal 中的元素可以是任意的表达式，不一定非得是常量；
+ */
+console.log("\n-------------------------------------------------- 02");
+const num = 0;
+const array04 = [num, num + 1, num - 1, num * num];
+/* [ 0, 1, -1, 0 ] */
+console.log(array04);
+
+/*
+ * array literal 中的元素如果留空，则元素值为 undefined；
+ */
+console.log("\n-------------------------------------------------- 03");
+/* undefined */
+console.log([1, , 3][1]);
+
+/*
+ * array literal 中允许末尾加上一个逗号；
+ */
+console.log("\n-------------------------------------------------- 04");
+/* 2 */
+console.log([, ,].length);
 
 export {};
