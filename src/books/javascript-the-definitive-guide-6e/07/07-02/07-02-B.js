c6d662239e88b141e18cddc969973b6f05607d91
luotao <luotaoyeah@gmail.com>
2018-05-26 08:57:38 +0800

    the definitive guide: 7.2: array is special object

---
+++ src/books/javascript-the-definitive-guide-6e/07/07-02/07-02-B.js
@@ -0,0 +1,26 @@
+/*
+ * array 是一种特殊的 object，使用 [] 访问 array element，类似于使用 [] 访问 object property；
+ * [] 中的 number 会被转换为 string；
+ */
+console.log("\n-------------------------------------------------- 01");
+
+const obj01 = {};
+obj01[0] = "x";
+obj01[1] = "y";
+/* { '0': 'x', '1': 'y' } */
+console.log(obj01);
+
+/*
+ * array 相比于 object 特殊的地方在于：
+ *     当 property name 是 non-negative number 时，array 会自动维护它的 length 属性；
+ */
+console.log("\n-------------------------------------------------- 02");
+
+const array01 = [1];
+/* 1 */
+console.log(array01.length);
+array01[2] = 3;
+/* 3 */
+console.log(array01.length);
+
+export {};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6e/07/07-02/07-02-A");
+require("./books/javascript-the-definitive-guide-6e/07/07-02/07-02-B");
