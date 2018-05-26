e7cadeb319bd73c1e20cc65b992b5ce5df24a944
luotao <luotaoyeah@gmail.com>
2018-05-26 10:20:55 +0800

    the definitive guide: 7.4: length

---
+++ src/books/javascript-the-definitive-guide-6e/07/07-04/07-04-A.js
@@ -0,0 +1,16 @@
+/*
+ * 7.4 Array Length
+ */
+
+/*
+ * 每一个 array 都有 length 属性，这是 array 和 object 最重要的区别；
+ * 对于 dense array，length 表示 element 的个数；
+ * 对于 sparse array，length 大于 element 的个数；
+ */
+console.log("\n-------------------------------------------------- 01");
+/* 3 */
+console.log([1, 2, 3].length);
+/* 3 */
+console.log([, , 3].length);
+
+export {};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6e/07/07-03/07-03-B");
+require("./books/javascript-the-definitive-guide-6e/07/07-04/07-04-A");
