985ec79ed91c7947a7c9f079ea65f187005bdc18
luotao <luotaoyeah@gmail.com>
2018-05-28 13:31:55 +0800

    the definitive guide: 7.8.7 Array.prototype.push(), Array.prototype.pop()

---
+++ src/books/javascript-the-definitive-guide-6e/07/07-08/07-08-07.js
@@ -0,0 +1,33 @@
+/*
+ * 7.8.7 Array.prototype.push()，Array.prototype.pop()
+ *
+ *
+ */
+
+(function() {
+  /*
+   * Array.prototype.push()：
+   *     往 array 的末尾插入一个或多个 element，返回 array 的 length；
+   */
+  console.log("\n-------------------------------------------------- 01");
+
+  const array01 = [];
+  /* 2 */
+  console.log(array01.push(1, 2));
+  /* [ 1, 2 ] */
+  console.log(array01);
+})();
+
+(function() {
+  /*
+   * Array.prototype.pop()：
+   *     删除 array 的最后一个 element，并返回该 element；
+   */
+  console.log("\n-------------------------------------------------- 02");
+
+  const array01 = [1, 2, 3];
+  /* 3 */
+  console.log(array01.pop());
+  /* [ 1, 2 ] */
+  console.log(array01);
+})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6e/07/07-08/07-08-06");
+require("./books/javascript-the-definitive-guide-6e/07/07-08/07-08-07");
