9c992667b7d330518ea091447a317397ee37683d
luotao <luotaoyeah@gmail.com>
2018-05-28 18:15:57 +0800

    the definitive guide: 7.9.5 Array.prototype.reduceRight()

---
+++ src/books/javascript-the-definitive-guide-6e/07/07-09/07-09-05/07-09-05-D.js
@@ -0,0 +1,22 @@
+(function() {
+  /*
+   * Array.prototype.reduceRight() 类似 Array.prototype.reduce()，
+   * 区别在于从右往左遍历；
+   */
+  console.log("\n-------------------------------------------------- 01");
+  const array01 = [1, 2, 3];
+
+  /* 1-2-3 */
+  console.log(
+    array01.reduce((accumulator, value) => {
+      return `${accumulator}-${value}`;
+    })
+  );
+
+  /* 3-2-1 */
+  console.log(
+    array01.reduceRight((accumulator, value) => {
+      return `${accumulator}-${value}`;
+    })
+  );
+})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6e/07/07-09/07-09-05/07-09-05-C");
+require("./books/javascript-the-definitive-guide-6e/07/07-09/07-09-05/07-09-05-D");
