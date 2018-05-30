385fd843385da4f36198b206fd055d1803d1666f
luotao <luotaoyeah@gmail.com>
2018-05-30 21:58:04 +0800

    the definitive guide: 8.1.1: function declaration statements are not true statements

---
+++ src/books/javascript-the-definitive-guide-6e/08/08-01/08-01-01/08-01-01-B.js
@@ -0,0 +1,27 @@
+(function() {
+  /*
+   * TODO 待验证
+   * function declaration statement 不是真正的 statements，
+   * 它只能出现在最外层代码中，或者其他的 function 的函数体的最外层；
+   * 它不能出现在 for，while，if，try/catch，with 语句中；
+   */
+  console.log("\n-------------------------------------------------- 01");
+
+  if (1 < 2) {
+    function fn01() {}
+  }
+
+  for (let i = 0; i < 10; i++, function fn02() {}) {
+    function fn03() {}
+  }
+
+  try {
+    throw new Error();
+  } catch (e) {
+    function fn04() {}
+  }
+
+  with ({ x: "x" }) {
+    function fn05() {}
+  }
+})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6e/08/08-01/08-01-01/08-01-01-A");
+require("./books/javascript-the-definitive-guide-6e/08/08-01/08-01-01/08-01-01-B");
