3909268ae8b583a3284eed586e01422e1f66c26c
luotao <luotaoyeah@gmail.com>
2018-06-04 21:50:51 +0800

    the definitive guide: 8.7: rest parameters of call() and apply()

---
+++ src/books/javascript-the-definitive-guide-6e/08/08-07/08-07-03/08-07-03-C.js
@@ -0,0 +1,31 @@
+(function() {
+  /*
+   * 对于 call()，从第二个参数开始的参数，会作为 function 的参数传入；
+   */
+  console.log("\n-------------------------------------------------- 01");
+
+  const obj01 = {
+    name: "foo"
+  };
+
+  (function(x, y) {
+    /* foo x y */
+    console.log(this.name, x, y);
+  }.call(obj01, "x", "y"));
+})();
+
+(function() {
+  /*
+   * 对于 apply()，第二个参数是一个 array，其每一个 element 会作为 function 的参数传入
+   */
+  console.log("\n-------------------------------------------------- 02");
+
+  const obj01 = {
+    name: "foo"
+  };
+
+  (function(x, y) {
+    /* foo x y */
+    console.log(this.name, x, y);
+  }.apply(obj01, ["x", "y"]));
+})();
