3b04e0f2fc9346b323f759e083a7a6041f3c0dda
luotao <luotaoyeah@gmail.com>
2018-06-05 21:42:32 +0800

    the definitive guide: 8.7.4: example 8-5

---
+++ src/books/javascript-the-definitive-guide-6e/08/08-07/08-07-04/example-8-5.js
@@ -0,0 +1,42 @@
+/*
+ * Example 8-5. A Function.prototype.bind() method for ECMAScript 3
+ */
+(function() {
+  console.log("\n-------------------------------------------------- 01");
+
+  /**
+   *
+   * @param obj
+   * @return {function(): *}
+   */
+  Function.prototype.bind01 = function(obj) {
+    const fn = this;
+    const arguments01 = arguments;
+
+    return function() {
+      const args = [];
+
+      for (let i = 1; i < arguments01.length; i++) {
+        args.push(arguments01[i]);
+      }
+
+      for (let i = 0; i < arguments.length; i++) {
+        args.push(arguments[i]);
+      }
+
+      return fn.apply(obj, args);
+    };
+  };
+
+  const obj01 = { x: 9 };
+
+  function fn01(y, z) {
+    return this.x + y + z;
+  }
+
+  const fn02 = fn01.bind(obj01, 9);
+  const fn03 = fn01.bind01(obj01, 9);
+
+  console.log(fn02(9));
+  console.log(fn03(9));
+})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6e/08/08-07/08-07-04/08-07-04-C");
+require("./books/javascript-the-definitive-guide-6e/08/08-07/08-07-04/example-8-5");
