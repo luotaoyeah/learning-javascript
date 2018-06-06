bb8334f3119f2deb91d11c54c5dec524469b8278
luotao <luotaoyeah@gmail.com>
2018-06-06 21:55:31 +0800

    the definitive guide: 8.7.5: Function.prototype.toString()

---
+++ src/books/javascript-the-definitive-guide-6e/08/08-07/08-07-05-A.js
@@ -0,0 +1,32 @@
+/*
+ * 8.7.5 The toString() Method
+ */
+
+(function() {
+  /*
+   * 对于自定义的 function，toString() 方法打印该 function 的源码；
+   */
+  console.log("\n-------------------------------------------------- 01");
+
+  function fn01() {
+    /* fn01 */
+  }
+
+  const fn02 = function() {
+    /* fn02 */
+  };
+
+  console.log(fn01.toString());
+  console.log(fn02.toString());
+})();
+
+(function() {
+  /*
+   * 对于内置的 function，toString() 方法打印该 function 的源码，
+   * 但是方法体为 '[native code]'；
+   */
+  console.log("\n-------------------------------------------------- 02");
+
+  /* function Date() { [native code] } */
+  console.log(Date.toString());
+})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6e/08/08-07/08-07-04/08-07-04-D");
+require("./books/javascript-the-definitive-guide-6e/08/08-07/08-07-05-A");
