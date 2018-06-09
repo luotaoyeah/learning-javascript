c3c4ddb344a6134781365324251571e631dd36c3
luotao <luotaoyeah@gmail.com>
2018-06-09 21:27:02 +0800

    the definitive guide: 9.3: example 9-3 [todo]

---
+++ src/books/javascript-the-definitive-guide-6e/09/09-03/example-9-3.js
@@ -0,0 +1,10 @@
+/*
+ * Example 9-3. Complex.js: A complex number class
+ */
+
+(function() {
+  /*
+   * TODO
+   */
+  console.log("\n-------------------------------------------------- 01");
+})();
