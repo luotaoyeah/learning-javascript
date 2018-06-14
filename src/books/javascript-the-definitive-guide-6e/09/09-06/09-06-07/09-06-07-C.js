1aa81c4dc14b545eb3a6c57e36ec57a3381bbe96
luotao <luotaoyeah@gmail.com>
2018-06-14 21:06:51 +0800

    the definitive guide: 9.6.7: different constructors share a prototype

---
+++ src/books/javascript-the-definitive-guide-6e/09/09-06/09-06-07/09-06-07-C.js
@@ -0,0 +1,26 @@
+(function() {
+  /*
+   * 多个 constructor 可以共享同一个 prototype 属性对象，
+   * 此时他们的实例对象属于相同的 type；
+   */
+  console.log("\n-------------------------------------------------- 01");
+
+  const proto = { x: "x" };
+
+  function A(name) {
+    this.name = name;
+  }
+
+  A.prototype = proto;
+
+  function B(age) {
+    this.age = age;
+  }
+
+  B.prototype = proto;
+
+  /* true */
+  console.log(new A("foo") instanceof B);
+  /* true */
+  console.log(new B(18) instanceof A);
+})();
