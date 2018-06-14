457469bd60d06dee23ce995ee1ed01cdd6eb7b22
luotao <luotaoyeah@gmail.com>
2018-06-14 22:04:52 +0800

    the definitive guide: 9.7.1: singleton set

---
+++ src/books/javascript-the-definitive-guide-6e/09/09-07/09-07-01/example-9-12.js
@@ -0,0 +1,46 @@
+/*
+ * Example 9-12. SingletonSet: a simple set subclass
+ */
+
+import { inherit } from "../../../06/06-01/06-01-04/example-6-1";
+import { Set } from "../../09-06/09-06-01/example-9-6";
+import { extend } from "../../../08/08-05/example-8-3";
+
+(function() {
+  /*
+   *
+   */
+  console.log("\n-------------------------------------------------- 01");
+
+  /**
+   * @param member
+   * @constructor
+   */
+  function SingletonSet(member) {
+    this.member = member;
+  }
+
+  SingletonSet.prototype = inherit(Set.prototype);
+
+  extend(SingletonSet.prototype, {
+    constructor: SingletonSet,
+    size: function() {
+      return 1;
+    },
+    add: function() {
+      throw new Error("readonly");
+    },
+    remove: function() {
+      throw new Error("readonly");
+    },
+    contains: function(x) {
+      return this.member === x;
+    }
+  });
+
+  const singletonSet = new SingletonSet(4);
+  /* false */
+  console.log(singletonSet.contains(5));
+  /* 1 */
+  console.log(singletonSet.size());
+})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6e/09/09-07/09-07-01/example-9-11");
+require("./books/javascript-the-definitive-guide-6e/09/09-07/09-07-01/example-9-12");
