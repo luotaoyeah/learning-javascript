d50dc973c791eab1afc3ff2d406a807a6a748a14
luotao <luotaoyeah@gmail.com>
2018-06-16 14:04:25 +0800

    the definitive guide: 9.7.4: example 9-16

---
+++ src/books/javascript-the-definitive-guide-6e/09/09-07/09-07-04/example-9-16.js
@@ -0,0 +1,25 @@
+/*
+ * Example 9-16. A hierarchy of abstract and concrete Set classes
+ * TODO
+ */
+
+import { defineSubClass } from "../09-07-01/example-9-11";
+import { Set } from "../../09-06/09-06-01/example-9-6";
+
+console.log("\n-------------------------------------------------- 01");
+
+/**
+ * 抽象方法，不能直接调用；
+ */
+function abstractMethod() {
+  throw new Error("abstract method");
+}
+
+function AbstractSet() {
+  throw new Error("cannot instantiate abstract class");
+}
+
+AbstractSet.prototype.contains = abstractMethod;
+AbstractSet.extend = function(constructor, methods, statics) {
+  return defineSubClass(AbstractSet, constructor, methods, statics);
+};
