6082624ebb388849a9ed5620e972bffd4a33280a
luotao <luotaoyeah@gmail.com>
2018-06-16 13:28:02 +0800

    the definitive guide: 9.7.3: example 9-15

--- src/books/javascript-the-definitive-guide-6e/09/09-07/09-07-01/example-9-11.js
+++ src/books/javascript-the-definitive-guide-6e/09/09-07/09-07-01/example-9-11.js
@@ -5,53 +5,53 @@
 import { inherit } from "../../../06/06-01/06-01-04/example-6-1";
 import { extend } from "../../../08/08-05/example-8-3";
 
-(function() {
-  /*
+/*
    *
    */
-  console.log("\n-------------------------------------------------- 01");
+console.log("\n-------------------------------------------------- 01");
 
-  /**
-   * 定义子类
-   * @param superClass 父类
-   * @param constructor 子类的 constructor
-   * @param methods 实例方法
-   * @param statics 静态方法
-   * @return {*}
-   */
-  function defineSubClass(superClass, constructor, methods, statics) {
-    constructor.prototype = inherit(superClass.prototype);
-    constructor.prototype.constructor = constructor;
+/**
+ * 定义子类
+ * @param superClass 父类
+ * @param constructor 子类的 constructor
+ * @param methods 实例方法
+ * @param statics 静态方法
+ * @return {*}
+ */
+function defineSubClass(superClass, constructor, methods, statics) {
+  constructor.prototype = inherit(superClass.prototype);
+  constructor.prototype.constructor = constructor;
 
-    if (methods) {
-      extend(constructor.prototype, methods);
-    }
-
-    if (statics) {
-      extend(constructor, statics);
-    }
-
-    return constructor;
+  if (methods) {
+    extend(constructor.prototype, methods);
   }
 
-  function A() {}
+  if (statics) {
+    extend(constructor, statics);
+  }
 
-  A.prototype.a = "a";
-  A.prototype.extend = function(constructor, methods, statics) {
-    return defineSubClass(A, constructor, methods, statics);
-  };
+  return constructor;
+}
 
-  function B() {}
+function A() {}
 
-  const B02 = new A().extend(B, {
-    c: function() {
-      return "c";
-    }
-  });
+A.prototype.a = "a";
+A.prototype.extend = function(constructor, methods, statics) {
+  return defineSubClass(A, constructor, methods, statics);
+};
 
-  const b02 = new B02();
-  /* a */
-  console.log(b02.a);
-  /* c */
-  console.log(b02.c());
-})();
+function B() {}
+
+const B02 = new A().extend(B, {
+  c: function() {
+    return "c";
+  }
+});
+
+const b02 = new B02();
+/* a */
+console.log(b02.a);
+/* c */
+console.log(b02.c());
+
+export { defineSubClass };
