06db5ea20351f7c3ef4ee4fd34d664d6b3120d4e
luotao <luotaoyeah@gmail.com>
2018-06-18 17:37:47 +0800

    the definitive guide: 9.9.2: example 9-24

--- src/books/javascript-the-definitive-guide-6e/09/09-09/09-09-02/09-09-02-A.js
+++ src/books/javascript-the-definitive-guide-6e/09/09-09/09-09-02/09-09-02-A.js
@@ -4,10 +4,10 @@
 
 (function() {
   /*
- * 通常我们在一个 module 中会定义很多的 methods 和 variables，
- * 其中有些是需要暴露给外部访问的，有些是不希望暴露给外部的；
- * 通常我们可以使用 function scope 来实现这样的效果；
- */
+   * 通常我们在一个 module 中会定义很多的 methods 和 variables，
+   * 其中有些是需要暴露给外部访问的，有些是不希望暴露给外部的；
+   * 通常我们可以使用 function scope 来实现这样的效果；
+   */
   console.log("\n-------------------------------------------------- 01");
 
   /*
@@ -23,6 +23,7 @@
     }
 
     function Obj() {}
+
     Obj.prototype.fn03 = function() {
       console.log(fn01() + ", " + fn02());
     };
