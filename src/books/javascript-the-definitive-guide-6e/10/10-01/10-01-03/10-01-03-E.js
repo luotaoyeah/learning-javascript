29652007eab01742dfe824e5b70d0991af9a1cbc
luotao <luotaoyeah@gmail.com>
2018-06-19 21:44:25 +0800

    the definitive guide: 10.1.3: ?

---
+++ src/books/javascript-the-definitive-guide-6e/10/10-01/10-01-03/10-01-03-E.js
@@ -0,0 +1,18 @@
+(function() {
+  /*
+   * ?
+   * 表示模式出现的次数为：0 == 次数 || 1 == 次数
+   */
+  console.log("\n-------------------------------------------------- 01");
+
+  const pattern01 = /x\d?/;
+
+  /* [ 'x', index: 0, input: 'x' ] */
+  console.log("x".match(pattern01));
+  /* [ 'x9', index: 0, input: 'x9x' ] */
+  console.log("x9x".match(pattern01));
+  /* [ 'x9', index: 0, input: 'x99x' ] */
+  console.log("x99x".match(pattern01));
+  /* [ 'x', index: 0, input: 'xx99xx' ] */
+  console.log("xx99xx".match(pattern01));
+})();
