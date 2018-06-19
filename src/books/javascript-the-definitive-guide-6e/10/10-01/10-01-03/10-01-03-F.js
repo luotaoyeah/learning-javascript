600067331099e2955477748062bdcb710904c7b1
luotao <luotaoyeah@gmail.com>
2018-06-19 21:48:51 +0800

    the definitive guide: 10.1.3: +

--- src/books/javascript-the-definitive-guide-6e/10/10-01/10-01-03/10-01-03-F.js
+++ src/books/javascript-the-definitive-guide-6e/10/10-01/10-01-03/10-01-03-F.js
@@ -1,36 +1,36 @@
 (function() {
   /*
-   * ?
-   * 表示模式出现的次数为：0 == 次数 || 1 == 次数
+   * +
+   * 表示模式出现的次数为：1 <= 次数
    */
   console.log("\n-------------------------------------------------- 01");
 
-  const pattern01 = /x\d?/;
+  const pattern01 = /x\d+/;
 
-  /* [ 'x', index: 0, input: 'x' ] */
+  /* null */
   console.log("x".match(pattern01));
   /* [ 'x9', index: 0, input: 'x9x' ] */
   console.log("x9x".match(pattern01));
-  /* [ 'x9', index: 0, input: 'x99x' ] */
+  /* [ 'x99', index: 0, input: 'x99x' ] */
   console.log("x99x".match(pattern01));
-  /* [ 'x', index: 0, input: 'xx99xx' ] */
+  /* [ 'x99', index: 1, input: 'xx99xx' ] */
   console.log("xx99xx".match(pattern01));
 })();
 
 (function() {
   /*
-   * ? 等价于 {0，1}
+   * + 等价于 {1,}
    */
   console.log("\n-------------------------------------------------- 02");
 
-  const pattern01 = /x\d{0,1}/;
+  const pattern01 = /x\d{1,}/;
 
-  /* [ 'x', index: 0, input: 'x' ] */
+  /* null */
   console.log("x".match(pattern01));
   /* [ 'x9', index: 0, input: 'x9x' ] */
   console.log("x9x".match(pattern01));
-  /* [ 'x9', index: 0, input: 'x99x' ] */
+  /* [ 'x99', index: 0, input: 'x99x' ] */
   console.log("x99x".match(pattern01));
-  /* [ 'x', index: 0, input: 'xx99xx' ] */
+  /* [ 'x99', index: 1, input: 'xx99xx' ] */
   console.log("xx99xx".match(pattern01));
 })();
