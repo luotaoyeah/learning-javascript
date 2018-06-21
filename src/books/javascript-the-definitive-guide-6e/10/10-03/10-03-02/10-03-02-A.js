7276b10781fe0f422b070732ab52fa85fe327ea5
luotao <luotaoyeah@gmail.com>
2018-06-21 17:24:21 +0800

    the definitive guide: 10.3.2: RegExp.prototype.test()

---
+++ src/books/javascript-the-definitive-guide-6e/10/10-03/10-03-02/10-03-02-A.js
@@ -0,0 +1,15 @@
+/*
+ * 10.3.2  RegExp Methods
+ */
+
+/*
+ * RegExp.prototype.test()
+ * RegExp.prototype.exec()
+ */
+
+(function() {
+  /*
+   *
+   */
+  console.log("\n-------------------------------------------------- 01");
+})();
