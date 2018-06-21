bf3218b1d613db1866669ecdc0f1c2f9dafa3cb8
luotao <luotaoyeah@gmail.com>
2018-06-21 21:19:15 +0800

    the definitive guide: 10.1.5: /g

---
+++ src/books/javascript-the-definitive-guide-6e/10/10-01/10-01-06/10-01-06-B.js
@@ -0,0 +1,14 @@
+(function() {
+  /*
+   * 标识元素 g 表示全局搜索，即找出目标字符串中的所有匹配项；
+   */
+  console.log("\n-------------------------------------------------- 01");
+
+  const pattern01 = /x/;
+  const pattern02 = /x/g;
+
+  /* [ 'x', index: 0, input: 'x9x9x9' ] */
+  console.log("x9x9x9".match(pattern01));
+  /* [ 'x', 'x', 'x' ] */
+  console.log("x9x9x9".match(pattern02));
+})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6e/10/10-01/10-01-06/10-01-06-A");
+require("./books/javascript-the-definitive-guide-6e/10/10-01/10-01-06/10-01-06-B");
