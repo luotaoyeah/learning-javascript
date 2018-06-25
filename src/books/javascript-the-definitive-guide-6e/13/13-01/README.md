a9ab9786bf57b83d16fab49838abda9ffbfae2d4
luotao <luotaoyeah@gmail.com>
2018-06-25 17:21:46 +0800

    the definitive guide: 13.1: window.location

---
+++ src/books/javascript-the-definitive-guide-6e/13/13-01/README.md
@@ -0,0 +1,1 @@
+## 13.1 Client-Side JavaScript
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/javascript-the-definitive-guide-6e/10/10-03/10-03-02/10-03-02-D");
+require("./books/javascript-the-definitive-guide-6e/13/13-01/13-01-A.js");
