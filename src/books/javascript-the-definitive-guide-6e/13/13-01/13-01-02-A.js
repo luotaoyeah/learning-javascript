b73f7b44db4d1b18cd99a219a630a2ffd5a3452f
luotao <luotaoyeah@gmail.com>
2018-06-25 22:21:11 +0800

    the definitive guide: 13.1.2: javascript in web applications

---
+++ src/books/javascript-the-definitive-guide-6e/13/13-01/13-01-02-A.js
@@ -0,0 +1,15 @@
+/*
+ * 13.1.2 JavaScript in Web Applications
+ */
+(function() {
+  /*
+   * 我们把浏览器比作一个操作系统，它提供了很多底层的 API 服务；
+   * 例如：网络，图形，存储等；
+   * 例如 XMLHttpRequest 用来在不刷新页面的情况下访问网络，获取数据；
+   * 例如 HTML5 规范定义的很多音视频相关的 API 等；
+   *
+   * 基于这些 API 的应用称之为 web application；
+   * 当禁用 JS 时，web application 将无法正常工作；
+   */
+  console.log("\n-------------------------------------------------- 01");
+})();
