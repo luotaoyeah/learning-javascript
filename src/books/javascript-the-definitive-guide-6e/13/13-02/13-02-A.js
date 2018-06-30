5aea1aebc8143d4c0b04774a633f06d1652d65a6
luotao <luotaoyeah@gmail.com>
2018-06-30 14:17:12 +0800

    the definitive guide: 13.2.4: return non-undefined value

--- src/books/javascript-the-definitive-guide-6e/13/13-02/13-02-A.js
+++ src/books/javascript-the-definitive-guide-6e/13/13-02/13-02-A.js
@@ -8,7 +8,7 @@
    *     1. 写在 <script>alert('hello');</script> 标签中间；
    *     2. 写在单独的 JS 文件，然后通过 <script src="index.js"></script> 标签的 src 属性引入；
    *     3. 在 HTML 元素的事件处理函数属性值中执行：<button onclick="alert('hello');"></button>；
-   *     4. 在 <a href="javascript:void(0)"></a> 标签的 href 属性值中，通过 javascript: 协议执行；
+   *     4. 在 URL 中，通过 javascript: 协议执行；
    */
   console.log("\n-------------------------------------------------- 01");
 })();
