da34a7477d5159da563245084d2093829eb66519
luotao <luotaoyeah@gmail.com>
2018-07-31 20:51:38 +0800

    the definitive guide: 18.1.1: using XMLHttpRequest

--- src/books/javascript-the-definitive-guide-6e/18/server.js
+++ src/books/javascript-the-definitive-guide-6e/18/server.js
@@ -6,10 +6,13 @@ var http = require("http");
 
 http
   .createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/json" });
+    response.writeHead(200, {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    });
     response.end("{message: 'hello word'}");
   })
   .listen(8888);
 
 console.log("\n--------------------------------------------------");
-console.log("\nserver is running at http://localhost:8080");
+console.log("server is running at http://localhost:8888");
