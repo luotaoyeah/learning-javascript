cee48857b98281ab0037f8db09cad69f776cdaeb
luotao <luotaoyeah@gmail.com>
2018-08-01 20:52:33 +0800

    the definitive guide: 18.1.2.2: example 18-3

--- src/books/javascript-the-definitive-guide-6e/18/server.js
+++ src/books/javascript-the-definitive-guide-6e/18/server.js
@@ -6,13 +6,26 @@ var http = require("http");
 
 http
   .createServer((request, response) => {
-    response.writeHead(200, {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/xhtml+xml"
-    });
-    response.end(
-      "<html><head><title>foo</title></head><body>bar</body></html>"
-    );
+    if (request.url.startsWith("/json")) {
+      response.writeHead(200, {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      });
+      response.end(
+        JSON.stringify({
+          name: "foo",
+          age: 18
+        })
+      );
+    } else if (request.url.startsWith("/xml")) {
+      response.writeHead(200, {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/xhtml+xml"
+      });
+      response.end(
+        "<html><head><title>foo</title></head><body>bar</body></html>"
+      );
+    }
   })
   .listen(8888);
 
