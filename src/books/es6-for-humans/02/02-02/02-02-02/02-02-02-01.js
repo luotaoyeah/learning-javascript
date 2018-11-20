fd0cedb291050b927834592f24d85891b06c0a40
luotao <luotaoyeah@gmail.com>
2018-11-20 19:09:48 +0800

    es6 for humans: es6: arrow function: function invocation

---
+++ src/books/es6-for-humans/02/02-02/02-02-02/02-02-02-01.js
@@ -0,0 +1,27 @@
+/*
+ * New Syntax in ES6
+ *     Arrow Functions
+ *         A Tale about this
+ *             Function Invocation
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 外层 function 中的 this 指向 global（nodejs）/window（browser）；
+   */
+  (function getContext() {
+    console.log(this === global); // true
+  })();
+}
+
+console.log("\n-------------------------------------------------- 02");
+{
+  /*
+   * 严格模式下，外层 function 中的 this 为 undefined；
+   */
+  (function getContext() {
+    "use strict";
+    console.log(this === global); // false
+  })();
+}
