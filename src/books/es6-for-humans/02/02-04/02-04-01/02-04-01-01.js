b3a86424a79c2083e42cf9983e930f555eba3bfc
luotao <luotaoyeah@gmail.com>
2018-11-22 19:08:14 +0800

    es6 for humans: es6: rest and spread operators: rest symbol

---
+++ src/books/es6-for-humans/02/02-04/02-04-01/02-04-01-01.js
@@ -0,0 +1,23 @@
+/*
+ * New Syntax in ES6
+ *     Rest and Spread Operators
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * ES6 提供的 rest parameter 支持了可变参数，即参数的个数在定义函数的时候是不确定的；
+   */
+
+  /*
+   * 参数 collection 前面的 ... 称之为 rest symbol，
+   * 它的作用是：将余下的参数全部装到 collection 这个数组参数里面；
+   */
+  (function(id, ...collection) {
+    console.log(collection instanceof Array);
+  })("01", "A", "B", "C"); // true
+
+  (function(id, ...collection) {
+    console.log(collection);
+  })("01", "A", "B", "C"); // [ 'A', 'B', 'C' ]
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/es6-for-humans/02/02-03/02-03-01/02-03-01-05");
+require("./books/es6-for-humans/02/02-04/02-04-01/02-04-01-01");
