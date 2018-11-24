240d93d1aaba32897d3f217394d2e48cd105b59c
luotao <luotaoyeah@gmail.com>
2018-11-24 09:58:46 +0800

    es6 for humans: es6: object literal extensions: iterating with for...of

---
+++ src/books/es6-for-humans/02/02-05/02-05-02/02-05-02-03.js
@@ -0,0 +1,24 @@
+/*
+ * New Syntax in ES6
+ *     Object Literal Extensions
+ *         Iterating with for...of
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 对于 iterable 的对象（array，string，etc），可以使用新的 for...of 迭代方式；
+   */
+
+  const arr01 = ["a", "b", "c"];
+
+  /* 使用旧的 for 循环进行迭代 */
+  for (let i = 0; i < arr01.length; i++) {
+    console.log(arr01[i]);
+  }
+
+  /* 使用新的 for...of 循环进行迭代 */
+  for (let i of arr01) {
+    console.log(i);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/es6-for-humans/02/02-05/02-05-02/02-05-02-02");
+require("./books/es6-for-humans/02/02-05/02-05-02/02-05-02-03");
