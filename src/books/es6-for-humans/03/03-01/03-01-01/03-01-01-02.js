4e2b51e3cf6145f47be2eaa4e5cd4856a3f8ea5d
luotao <luotaoyeah@gmail.com>
2018-11-25 08:18:55 +0800

    es6 for humans: destructure: object destructuring syntax: shorthand

---
+++ src/books/es6-for-humans/03/03-01/03-01-01/03-01-01-02.js
@@ -0,0 +1,27 @@
+/*
+ * Destructuring
+ *     Destructuring of Objects and Arrays
+ *         Object Destructuring Syntax
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 对于 object destructuring，当变量名跟属性名相同时，可以简写；
+   */
+
+  const { a: a, b: b, c: c } = {
+    a: 1,
+    b: 2,
+    c: 3
+  };
+  console.log(a, b, c); // 1 2 3
+
+  /* 简写 */
+  const { x, y, z } = {
+    x: 1,
+    y: 2,
+    z: 3
+  };
+  console.log(x, y, z); // 1 2 3
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/es6-for-humans/03/03-01/03-01-01/03-01-01-01");
+require("./books/es6-for-humans/03/03-01/03-01-01/03-01-01-02");
