a7a6d5298d29ac512fe951ccfce99b3267bc9104
luotao <luotaoyeah@gmail.com>
2018-12-02 21:16:49 +0800

    es6 for humans: classes: class inheritance and the super keyword

---
+++ src/books/es6-for-humans/04/04-08/04-08-01/04-08-01-02.js
@@ -0,0 +1,31 @@
+/*
+ * Classes in ES6
+ *     Class Inheritance and the Super Keyword
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 当子类中没有定义构造函数时，会自动创建一个构造函数，
+   * 该构造函数中会自动调用 super() 方法，
+   * 调用 super() 方法时的参数为传入该构造函数的参数
+   */
+
+  class Person {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+
+  class Boy extends Person {}
+
+  console.log(new Boy("foo").name); // foo
+
+  class Girl extends Person {
+    constructor(...args) {
+      super(...args);
+    }
+  }
+
+  console.log(new Girl("bar").name); // bar
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./books/es6-for-humans/04/04-08/04-08-01/04-08-01-01");
+require("./books/es6-for-humans/04/04-08/04-08-01/04-08-01-02");
