67fa0303f132b443a0734ba7539723a90e11f91a
luotao <luotaoyeah@gmail.com>
2018-12-03 20:19:43 +0800

    es6 for humans: modules: exporting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-require("./books/es6-for-humans/05-modules/05-02/05-02-01/05-02-01-01");
+import("./books/es6-for-humans/05-modules/05-04/05-04-01/05-04-01-01").then(
+  ({ getMore }) => {
+    getMore(); // more
+  }
+);
