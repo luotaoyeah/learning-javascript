59c668cb06eedd95c7999fc4cebd3d4cde326992
luotao <luotaoyeah@gmail.com>
2018-12-03 21:28:19 +0800

    es6 for humans: modules: default exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import("./books/es6-for-humans/05-modules/05-04/05-04-01/05-04-01-03").then(
-  ({ default: getSome }) => {
-    getSome(); // SOME
+import("./books/es6-for-humans/05-modules/05-04/05-04-01/05-04-01-04").then(
+  ({ default: fn, name, age }) => {
+    fn(); // DEFAULT
+    console.log(name); // FOO
+    console.log(age); // 18
   }
 );
