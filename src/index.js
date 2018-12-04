198823251ffe42b2649f1d1d129bd791f5c90a74
luotao <luotaoyeah@gmail.com>
2018-12-04 19:40:03 +0800

    es6 for humans: modules: importing: default export as named export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/05-modules/05-05/05-05-02/05-05-02-02")
+import("./books/es6-for-humans/05-modules/05-05/05-05-02/05-05-02-03")
   .then(() => {})
   .catch(e => {
     console.error(e.message);
