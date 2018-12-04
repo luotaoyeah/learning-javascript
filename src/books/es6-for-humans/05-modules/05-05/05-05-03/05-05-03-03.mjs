8a5284410710d2080d3f8db5ce6d3565afea21b1
luotao <luotaoyeah@gmail.com>
2018-12-04 20:03:00 +0800

    es6 for humans: modules: importing: export *

---
+++ src/books/es6-for-humans/05-modules/05-05/05-05-03/05-05-03-03.mjs
@@ -0,0 +1,9 @@
+/*
+ * Modules
+ *     Importing
+ *         Exporting an Imported Binding
+ */
+
+console.log("\n-------------------------------------------------- 01");
+import {fn}  from "./05-05-03-02";
+fn();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/05-modules/05-05/05-05-03/05-05-03-02")
+import("./books/es6-for-humans/05-modules/05-05/05-05-03/05-05-03-03")
   .then(() => {})
   .catch(e => {
     console.error(e.message);
