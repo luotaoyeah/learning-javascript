c7bd0fc07a4979d744f5742f9436c3015e92a822
luotao <luotaoyeah@gmail.com>
2018-12-04 19:48:47 +0800

    es6 for humans: modules: importing: re-export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/05-modules/05-05/05-05-02/05-05-02-03")
+import("./books/es6-for-humans/05-modules/05-05/05-05-03/05-05-03-02")
   .then(() => {})
   .catch(e => {
     console.error(e.message);
