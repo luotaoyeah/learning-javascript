0431a48f4bee00299819c5f1dbd3428c1dc90f83
luotao <luotaoyeah@gmail.com>
2018-12-04 18:50:30 +0800

    es6 for humans: modules: importing: execute once

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/05-modules/05-05/05-05-01/05-05-01-02")
+import("./books/es6-for-humans/05-modules/05-05/05-05-01/05-05-01-03")
   .then(() => {})
   .catch(e => {
     console.error(e.message);
