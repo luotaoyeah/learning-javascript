44958cfb065d48f450fa6e0a6907e07108b99e16
luotao <luotaoyeah@gmail.com>
2018-12-04 18:20:57 +0800

    es6 for humans: modules: importing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import("./books/es6-for-humans/05-modules/05-04/05-04-01/05-04-01-04").then(
-  ({ default: fn, name, age }) => {
-    fn(); // DEFAULT
-    console.log(name); // FOO
-    console.log(age); // 18
-  }
-);
+import("./books/es6-for-humans/05-modules/05-05/05-05-01/05-05-01-01")
+  .then(() => {})
+  .catch(e => {
+    console.error(e.message);
+  });
