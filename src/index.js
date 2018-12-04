e17d372b5ef6ef5ceb30af1cf3dc8ab7fb16c342
luotao <luotaoyeah@gmail.com>
2018-12-04 18:33:02 +0800

    es6 for humans: modules: importing: asterisk

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/05-modules/05-05/05-05-01/05-05-01-01")
+import("./books/es6-for-humans/05-modules/05-05/05-05-01/05-05-01-02")
   .then(() => {})
   .catch(e => {
     console.error(e.message);
