394b61bcb8f97c950c4e5a62e78e23ccdc49bc66
luotao <luotaoyeah@gmail.com>
2018-12-04 21:09:38 +0800

    es6 for humans: modules: importing: renaming import

---
+++ src/books/es6-for-humans/05-modules/05-05/05-05-04/05-05-04-02.mjs
@@ -0,0 +1,28 @@
+/*
+ * Modules
+ *     Importing
+ *         Renaming Identifiers
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * 在引入的时候，也可以使用 as 给引入的 variable（function，class）起一个别名
+ */
+
+import { name as NAME, age as AGE } from "../../05-04/05-04-01/05-04-01-04";
+
+console.log(NAME); // FOO
+console.log(AGE); // 18
+
+console.log("\n-------------------------------------------------- 02");
+/*
+ * 引入默认输出有三种方式：直接引入，使用别名引入，使用 namespace 引入
+ */
+
+import fn01 from "../../05-04/05-04-01/05-04-01-04";
+import { default as fn02 } from "../../05-04/05-04-01/05-04-01-04";
+import * as obj from "../../05-04/05-04-01/05-04-01-04";
+
+fn01(); // DEFAULT
+fn02(); // DEFAULT
+obj.default(); // DEFAULT
