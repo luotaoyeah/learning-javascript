52b2c2c90ce18cb783ff870d1b4518183792fff7
luotao <luotaoyeah@gmail.com>
2018-12-04 20:05:30 +0800

    es6 for humans: modules: importing: re-export default as named

---
+++ src/books/es6-for-humans/05-modules/05-05/05-05-03/05-05-03-04.mjs
@@ -0,0 +1,10 @@
+/*
+ * Modules
+ *     Importing
+ *         Exporting an Imported Binding
+ */
+
+console.log("\n-------------------------------------------------- 01");
+import { DEFAULT } from "./05-05-03-03";
+
+DEFAULT();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/05-modules/05-05/05-05-03/05-05-03-03")
+import("./books/es6-for-humans/05-modules/05-05/05-05-03/05-05-03-04")
   .then(() => {})
   .catch(e => {
     console.error(e.message);
