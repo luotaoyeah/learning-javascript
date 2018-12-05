359d4a81d4386c54489c2e0c5f6760c1138473ff
luotao <luotaoyeah@gmail.com>
2018-12-05 21:01:30 +0800

    es6 for humans: modules: pitfall: syntax

---
+++ src/books/es6-for-humans/05-modules/05-08/05-08-01/05-08-01-01.mjs
@@ -0,0 +1,15 @@
+/*
+ * Modules
+ *     Common Pitfalls
+ *         Syntax
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * import 和 export 语句只能出现在模块文件的最顶层，
+ * 不能出现在其他代码块中
+ */
+
+{
+  export const name = "FOO"; // Unexpected token export
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/05-modules/05-05/05-05-04/05-05-04-02")
+import("./books/es6-for-humans/05-modules/05-08/05-08-01/05-08-01-01")
   .then(() => {})
   .catch(e => {
     console.error(e.message);
