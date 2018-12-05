34198bce1eb47d905d5794fbcbdc81cc14d0c03f
luotao <luotaoyeah@gmail.com>
2018-12-05 21:13:27 +0800

    es6 for humans: modules: pitfall: read-only bindings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import("./books/es6-for-humans/05-modules/05-08/05-08-01/05-08-01-01")
+import("./books/es6-for-humans/05-modules/05-08/05-08-01/05-08-01-02")
   .then(() => {})
   .catch(e => {
-    console.error(e.message);
+    console.error(e);
   });
