948e6e5ff0eeaa69b61994420e57af4af838e30d
luotao <luotaoyeah@gmail.com>
2018-12-05 18:04:19 +0800

    es6 for humans: modules: modules in web browsers

---
+++ src/books/es6-for-humans/05-modules/05-07/05-07-01/05-07-01-01.js
@@ -0,0 +1,8 @@
+/*
+ * Modules
+ *     Modules in Web Browsers
+ *         Loading Modules with <script>
+ */
+
+console.log("\n-------------------------------------------------- 01");
+export const name = "BAR";
