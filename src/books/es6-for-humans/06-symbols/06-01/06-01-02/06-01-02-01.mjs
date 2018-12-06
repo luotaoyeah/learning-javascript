7a83d581fc11a821c0649f34218438c76ff3f0ea
luotao <luotaoyeah@gmail.com>
2018-12-06 21:14:49 +0800

    es6 for humans: symbol: as prop name

---
+++ src/books/es6-for-humans/06-symbols/06-01/06-01-02/06-01-02-01.mjs
@@ -0,0 +1,22 @@
+/*
+ * Symbols in ES6
+ *     Symbols
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * symbol 的主要用途：用作属性名称
+ */
+const obj = {
+  name: "FOO",
+  say() {},
+  [Symbol()]() {}
+};
+console.log(obj); // { name: 'FOO', say: [Function: say], [Symbol()]: [Function] }
+
+console.log("\n-------------------------------------------------- 02");
+/*
+ * 如果一个属性名称是 symbol
+ * 则该属性是 non-enumerable 的
+ */
+console.log(JSON.stringify({ [Symbol()]: "ANONYMOUS" })); // {}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/06-symbols/06-01/06-01-01/06-01-01-04")
+import("./books/es6-for-humans/06-symbols/06-01/06-01-02/06-01-02-01")
   .then(() => {})
   .catch(e => {
     console.error(e);
