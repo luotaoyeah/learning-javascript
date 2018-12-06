be0c93b80a26e9bdbfd522ecad156246154d3e71
luotao <luotaoyeah@gmail.com>
2018-12-06 20:55:34 +0800

    es6 for humans: symbol: unique

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/06-symbols/06-01/06-01-01/06-01-01-03")
+import("./books/es6-for-humans/06-symbols/06-01/06-01-01/06-01-01-04")
   .then(() => {})
   .catch(e => {
     console.error(e);
