b38cc5211acceeeec7f75ba921bfb338a894fbee
luotao <luotaoyeah@gmail.com>
2018-12-06 20:52:03 +0800

    es6 for humans: symbol: no new

---
+++ src/books/es6-for-humans/06-symbols/06-01/06-01-01/06-01-01-03.mjs
@@ -0,0 +1,12 @@
+/*
+ * Symbols in ES6
+ *     Symbols
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * Symbol() 是一个工厂方法，不是一个构造方法，
+ * 因此不能使用 new
+ */
+
+const symbol = new Symbol("FOO"); // TypeError: Symbol is not a constructor
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/06-symbols/06-01/06-01-01/06-01-01-02")
+import("./books/es6-for-humans/06-symbols/06-01/06-01-01/06-01-01-03")
   .then(() => {})
   .catch(e => {
     console.error(e);
