e0fac73448ee2bfef51f7020d635cff1e56b06f2
luotao <luotaoyeah@gmail.com>
2018-12-07 19:05:34 +0800

    es6 for humans: symbol: Symbol.keyFor()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/06-symbols/06-03/06-03-01/06-03-01-01")
+import("./books/es6-for-humans/06-symbols/06-04/06-04-01/06-04-01-01")
   .then(() => {})
   .catch(e => {
     console.error(e);
