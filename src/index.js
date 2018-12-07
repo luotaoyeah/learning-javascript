17ee4199cef742cdb847cf888a24c054d5b9d6ab
luotao <luotaoyeah@gmail.com>
2018-12-07 20:24:35 +0800

    es6 for humans: array: Array.from()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/07-array/07-01/07-01-01/07-01-01-01")
+import("./books/es6-for-humans/07-array/07-01/07-01-01/07-01-01-02")
   .then(() => {})
   .catch(e => {
     console.error(e);
