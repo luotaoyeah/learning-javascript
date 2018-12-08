483205755d41e549174410a381f176e7796f5cc4
luotao <luotaoyeah@gmail.com>
2018-12-08 17:24:31 +0800

    es6 for humans: array: Array.prototype.find()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/07-array/07-01/07-01-03/07-01-03-04")
+import("./books/es6-for-humans/07-array/07-01/07-01-03/07-01-03-05")
   .then(() => {})
   .catch(e => {
     console.error(e);
