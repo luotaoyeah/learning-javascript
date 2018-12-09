b53ffa79f8469f92177322e18e562f61265734d9
luotao <luotaoyeah@gmail.com>
2018-12-09 17:36:05 +0800

    es6 for humans: array: WeakSet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/07-array/07-04/07-04-01/07-04-01-02")
+import("./books/es6-for-humans/07-array/07-04/07-04-01/07-04-01-03")
   .then(() => {})
   .catch(e => {
     console.error(e);
