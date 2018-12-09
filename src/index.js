fd5f590912a6d100480682b4b561cbf80490f61d
luotao <luotaoyeah@gmail.com>
2018-12-09 12:07:30 +0800

    es6 for humans: array: typed array: RangeError

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/07-array/07-02/07-02-02/07-02-02-01")
+import("./books/es6-for-humans/07-array/07-02/07-02-02/07-02-02-02")
   .then(() => {})
   .catch(e => {
     console.error(e);
