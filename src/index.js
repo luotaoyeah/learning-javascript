701fdc6650120afdc6f571564bba170a2b4a3c0a
luotao <luotaoyeah@gmail.com>
2018-12-09 15:17:55 +0800

    es6 for humans: array: map: checking keys equality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/07-array/07-03/07-03-01/07-03-01-01")
+import("./books/es6-for-humans/07-array/07-03/07-03-01/07-03-01-02")
   .then(() => {})
   .catch(e => {
     console.error(e);
