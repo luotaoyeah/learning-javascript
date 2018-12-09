1a71e3529411aa8a1e5f21868bafd48c5aa014eb
luotao <luotaoyeah@gmail.com>
2018-12-09 15:29:54 +0800

    es6 for humans: array: map: initialize with iterable k/v

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/07-array/07-03/07-03-01/07-03-01-02")
+import("./books/es6-for-humans/07-array/07-03/07-03-01/07-03-01-03")
   .then(() => {})
   .catch(e => {
     console.error(e);
