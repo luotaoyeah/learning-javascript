3e1e4effd28fedf333e9aa69359c457c72664c2a
luotao <luotaoyeah@gmail.com>
2018-12-09 17:27:11 +0800

    es6 for humans: array: Set: forEach()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/07-array/07-04/07-04-01/07-04-01-01")
+import("./books/es6-for-humans/07-array/07-04/07-04-01/07-04-01-02")
   .then(() => {})
   .catch(e => {
     console.error(e);
