b4749c5ef05be26ea29519a1f3912bc1c9a3c6fb
luotao <luotaoyeah@gmail.com>
2018-12-09 15:35:25 +0800

    es6 for humans: array: map: entries(), keys(), values()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/07-array/07-03/07-03-01/07-03-01-03")
+import("./books/es6-for-humans/07-array/07-03/07-03-01/07-03-01-04")
   .then(() => {})
   .catch(e => {
     console.error(e);
