9e0d6af2f89c5f5c140fb58ec61b896a29441e54
luotao <luotaoyeah@gmail.com>
2018-12-10 18:11:19 +0800

    es6 for humans: iterator & generator: iterable protocol

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/08-iterator-and-generator/08-01/08-01-01/08-01-01-01")
+import("./books/es6-for-humans/08-iterator-and-generator/08-01/08-01-01/08-01-01-02")
   .then(() => {})
   .catch(e => {
     console.error(e);
