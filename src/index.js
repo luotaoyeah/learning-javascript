04274bd470d62dd9bf4515c3eff2a591a90b1da3
luotao <luotaoyeah@gmail.com>
2018-12-12 20:18:59 +0800

    es6 for humans: iterator & generator: communicating with generators: async

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/08-iterator-and-generator/08-06/08-06-01/08-06-01-01")
+import("./books/es6-for-humans/08-iterator-and-generator/08-06/08-06-01/08-06-01-02")
   .then(() => {})
   .catch(e => {
     console.error(e);
