88f06510b89710075bc476205ae492a912ba6f4a
luotao <luotaoyeah@gmail.com>
2018-12-12 21:38:08 +0800

    es6 for humans: iterator & generator: Iterator.return()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/08-iterator-and-generator/08-06/08-06-01/08-06-01-02")
+import("./books/es6-for-humans/08-iterator-and-generator/08-07/08-07-01/08-07-01-01")
   .then(() => {})
   .catch(e => {
     console.error(e);
