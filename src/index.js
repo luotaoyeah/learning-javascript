775e518ae12231b7e53688dd6b3cb4ff59566b09
luotao <luotaoyeah@gmail.com>
2018-12-12 19:32:03 +0800

    es6 for humans: iterator & generator: generator function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/08-iterator-and-generator/08-04/08-04-01/08-04-01-01")
+import("./books/es6-for-humans/08-iterator-and-generator/08-05/08-05-01/08-05-01-01")
   .then(() => {})
   .catch(e => {
     console.error(e);
