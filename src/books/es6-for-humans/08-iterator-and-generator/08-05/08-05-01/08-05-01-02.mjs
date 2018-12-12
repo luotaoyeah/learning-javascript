d0060f6a8c4af5704e14e06e780937fbc015fa5f
luotao <luotaoyeah@gmail.com>
2018-12-12 19:35:59 +0800

    es6 for humans: iterator & generator: generator function: params

---
+++ src/books/es6-for-humans/08-iterator-and-generator/08-05/08-05-01/08-05-01-02.mjs
@@ -0,0 +1,21 @@
+/*
+ * Iterators and Generators
+ *     Generator Function
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * generator function 同样可以接受参数
+ */
+
+function* gen(name) {
+  yield 1;
+  yield name.toUpperCase();
+  yield 3;
+}
+
+const iterator = gen("foo");
+console.log(iterator.next());
+console.log(iterator.next()); // { value: 'FOO', done: false }
+
+export {};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/08-iterator-and-generator/08-05/08-05-01/08-05-01-01")
+import("./books/es6-for-humans/08-iterator-and-generator/08-05/08-05-01/08-05-01-02")
   .then(() => {})
   .catch(e => {
     console.error(e);
