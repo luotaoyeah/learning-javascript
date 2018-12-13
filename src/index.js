b56331d17cba3c17c7e25d43a41426aa7dc32663
luotao <luotaoyeah@gmail.com>
2018-12-13 19:39:40 +0800

    es6 for humans: promise: Promise.resolve(), Promise.reject()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/09-promise/09-02/09-02-01/09-02-01-01")
+import("./books/es6-for-humans/09-promise/09-02/09-02-01/09-02-01-02")
   .then(() => {})
   .catch(e => {
     console.error(e);
