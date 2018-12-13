019a993d0c011f4f946153b1ffbab0a808d5cca0
luotao <luotaoyeah@gmail.com>
2018-12-13 19:30:46 +0800

    es6 for humans: promise: fulfilled, rejected, pending

---
+++ src/books/es6-for-humans/09-promise/09-01/09-01-01/09-01-01-02.mjs
@@ -0,0 +1,19 @@
+/*
+ * Promise
+ *     Promises Overview
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * promise 对象有三种状态：pending，fulfilled，rejected
+ * 可以由 pending 变更为 fulfilled
+ * 也可以由 pending 变更为 rejected
+ *
+ * 当状态为 fulfilled 或者 rejected 时，称之为 settled
+ * 此时状态不能再变更
+ *
+ * fulfilled 表示成功
+ * rejected 表示失败
+ */
+
+export {};
