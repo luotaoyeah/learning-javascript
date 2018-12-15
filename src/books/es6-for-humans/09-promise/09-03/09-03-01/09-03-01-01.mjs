ea30afd79defcfccfae620f74a81cb3895cf63f3
luotao <luotaoyeah@gmail.com>
2018-12-15 16:54:22 +0800

    es6 for humans: promise: Promise.all()

---
+++ src/books/es6-for-humans/09-promise/09-03/09-03-01/09-03-01-01.mjs
@@ -0,0 +1,34 @@
+/*
+ * Promise
+ *     Combining Promises with Promises.all()
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * Promise.all() 方法，接收一个 promise 数组，返回一个新的 promise 对象，
+ * 当数组中所有的 promise 都 fulfilled 时，该 promise 就会 fulfilled
+ * 如果数组中某一个 promise 被 rejected 了，该 promise 就会 rejected
+ */
+
+const p01 = new Promise(resolve => {
+  setTimeout(() => {
+    console.log("EXE FOO");
+    resolve("FOO");
+  }, 1000);
+});
+
+const p02 = new Promise(resolve => {
+  setTimeout(() => {
+    console.log("EXE BAR");
+    resolve("BAR");
+  }, 2000);
+});
+
+Promise.all([p01, p02]).then(v => {
+  /*
+   * 返回结果为所有 promise 的结果组成的数组
+   */
+  console.log(v); // [ 'FOO', 'BAR' ]
+});
+
+export {};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/09-promise/09-02/09-02-04/09-02-04-02")
+import("./books/es6-for-humans/09-promise/09-03/09-03-01/09-03-01-01")
   .then(() => {})
   .catch(e => {
     console.error(e);
