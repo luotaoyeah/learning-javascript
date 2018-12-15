b6eeec4ee615eb56af4cb6a25f321b83fb3c7c46
luotao <luotaoyeah@gmail.com>
2018-12-15 17:07:16 +0800

    es6 for humans: promise: Promise.all().catch()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/09-promise/09-03/09-03-01/09-03-01-01")
+import("./books/es6-for-humans/09-promise/09-03/09-03-01/09-03-01-02")
   .then(() => {})
   .catch(e => {
     console.error(e);
