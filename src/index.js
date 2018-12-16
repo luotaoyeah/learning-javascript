00d6bcc314d3bdf0883b1f180b15e07984f34df2
luotao <luotaoyeah@gmail.com>
2018-12-16 20:12:49 +0800

    es6 for humans: meta-programming: Reflect.ownKeys()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/10-meta-programming/10-04/10-04-02/10-04-02-01")
+import("./books/es6-for-humans/10-meta-programming/10-04/10-04-02/10-04-02-02")
   .then(() => {})
   .catch(e => {
     console.error(e);
