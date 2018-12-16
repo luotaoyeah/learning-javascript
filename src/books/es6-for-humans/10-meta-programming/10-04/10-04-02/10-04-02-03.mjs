c7f72006ad6a3affdd512eb3d58aad011532944a
luotao <luotaoyeah@gmail.com>
2018-12-16 20:24:49 +0800

    es6 for humans: meta-programming: Reflect.apply()

---
+++ src/books/es6-for-humans/10-meta-programming/10-04/10-04-02/10-04-02-03.mjs
@@ -0,0 +1,18 @@
+/*
+ * Meta Programming
+ *     Revocable Proxy
+ *         Reflect
+ *             Reflect.apply()
+ */
+
+console.log("\n-------------------------------------------------- 01");
+const obj = { name: "FOO" };
+function doSomething() {
+  return this.name;
+}
+
+console.log(doSomething.apply(obj)); // FOO
+console.log(Function.prototype.apply.call(doSomething, obj)); // FOO
+console.log(Reflect.apply(doSomething, obj, [])); // FOO
+
+export {};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/10-meta-programming/10-04/10-04-02/10-04-02-02")
+import("./books/es6-for-humans/10-meta-programming/10-04/10-04-02/10-04-02-03")
   .then(() => {})
   .catch(e => {
     console.error(e);
