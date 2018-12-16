18ac2ead7f1e368ffa8b022b694c34495cde8b51
luotao <luotaoyeah@gmail.com>
2018-12-16 18:34:20 +0800

    es6 for humans: meta-programming: ProxyHandler.ownKeys()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/10-meta-programming/10-03/10-03-01/10-03-01-04")
+import("./books/es6-for-humans/10-meta-programming/10-03/10-03-01/10-03-01-05")
   .then(() => {})
   .catch(e => {
     console.error(e);
