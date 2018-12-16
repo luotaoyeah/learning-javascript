e05cbfc8105fb56732d93c39fa2fb50e1f9b0aa8
luotao <luotaoyeah@gmail.com>
2018-12-16 21:24:52 +0800

    es6 for humans: es2016: String.prototype.padStart()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/11-beyond-es6/11-01/11-01-04/11-01-04-01")
+import("./books/es6-for-humans/11-beyond-es6/11-01/11-01-05/11-01-05-01")
   .then(() => {})
   .catch(e => {
     console.error(e);
