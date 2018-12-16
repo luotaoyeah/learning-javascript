1b80a2894810d1ea6ba619c1b7b46f8c23054eb8
luotao <luotaoyeah@gmail.com>
2018-12-16 19:28:40 +0800

    es6 for humans: meta-programming: ProxyHandler.apply()

---
+++ src/books/es6-for-humans/10-meta-programming/10-03/10-03-01/10-03-01-07.mjs
@@ -0,0 +1,33 @@
+/*
+ * Meta Programming
+ *     Traps in Proxy Handler
+ *         apply()
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * ProxyHandler.apply() 用来拦截函数调用
+ */
+
+function doSomething(name) {
+  return name + "BAR";
+}
+
+const proxy = new Proxy(doSomething, {
+  /*
+   * target 表示函数对象
+   * thisArg 表示 this 对象
+   * argArray 表示参数列表
+   */
+  apply(target, thisArg, argArray) {
+    if (typeof argArray[0] === "string") {
+      argArray[0] = argArray[0].toUpperCase();
+    }
+
+    return target.apply(thisArg, argArray);
+  }
+});
+
+console.log(proxy("foo")); // FOOBAR
+
+export {};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/10-meta-programming/10-03/10-03-01/10-03-01-06")
+import("./books/es6-for-humans/10-meta-programming/10-03/10-03-01/10-03-01-07")
   .then(() => {})
   .catch(e => {
     console.error(e);
