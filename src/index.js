ab1f0983d7ac6c3f12911105d7f081e06f43ec0d
luotao <luotaoyeah@gmail.com>
2018-12-16 17:55:39 +0800

    es6 for humans: meta-programming: ProxyHandler.set()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/10-meta-programming/10-03/10-03-01/10-03-01-01")
+import("./books/es6-for-humans/10-meta-programming/10-03/10-03-01/10-03-01-02")
   .then(() => {})
   .catch(e => {
     console.error(e);
