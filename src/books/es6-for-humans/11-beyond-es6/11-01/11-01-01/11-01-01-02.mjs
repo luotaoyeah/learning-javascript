e535c35b412da90c86c257685182ac24ac830c84
luotao <luotaoyeah@gmail.com>
2018-12-16 20:51:08 +0800

    es6 for humans: es2016: **

--- src/books/es6-for-humans/11-beyond-es6/11-01/11-01-01/11-01-01-02.mjs
+++ src/books/es6-for-humans/11-beyond-es6/11-01/11-01-01/11-01-01-02.mjs
@@ -6,7 +6,7 @@
 console.log("\n-------------------------------------------------- 01");
 /*
  * Array.prototype.includes() 方法和 indexOf() 方法的区别在于：
- *     对于 NaN 的不同处理方式
+ *     对于 NaN 的不同处理
  */
 
 console.log([Number.NaN].includes(Number.NaN)); // true
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/11-beyond-es6/11-01/11-01-01/11-01-01-02")
+import("./books/es6-for-humans/11-beyond-es6/11-01/11-01-02/11-01-02-01")
   .then(() => {})
   .catch(e => {
     console.error(e);
