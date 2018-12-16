acd1f05d0e3cd2fc20b03a1d2eb4719c4feeacad
luotao <luotaoyeah@gmail.com>
2018-12-16 20:46:46 +0800

    es6 for humans: es2016: Array.prototype.includes()

---
+++ src/books/es6-for-humans/11-beyond-es6/11-01/11-01-01/11-01-01-02.mjs
@@ -0,0 +1,15 @@
+/*
+ * Beyond ES6
+ *     The includes method on Arrays
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * Array.prototype.includes() 方法和 indexOf() 方法的区别在于：
+ *     对于 NaN 的不同处理方式
+ */
+
+console.log([Number.NaN].includes(Number.NaN)); // true
+console.log([Number.NaN].indexOf(Number.NaN) > -1); // false
+
+export {};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/11-beyond-es6/11-01/11-01-01/11-01-01-01")
+import("./books/es6-for-humans/11-beyond-es6/11-01/11-01-01/11-01-01-02")
   .then(() => {})
   .catch(e => {
     console.error(e);
