3b4a73c1a4ab597977c70a37e50d68daa3d30307
luotao <luotaoyeah@gmail.com>
2018-12-16 19:46:53 +0800

    es6 for humans: meta-programming: Proxy.revocable()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import("./books/es6-for-humans/10-meta-programming/10-03/10-03-01/10-03-01-07")
+import("./books/es6-for-humans/10-meta-programming/10-04/10-04-01/10-04-01-01")
   .then(() => {})
   .catch(e => {
     console.error(e);
