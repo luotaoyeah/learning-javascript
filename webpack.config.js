b1374dabafd78e7d1d928e774cefa9db185191ee
luotao <luotaoyeah@gmail.com>
2019-10-21 17:59:38 +0800

    chore: update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: ["@babel/polyfill", "./src/index.js"]
+    app: ['@babel/polyfill', './src/index.js'],
   },
   output: {
-    path: path.resolve("dist/"),
-    filename: "index.js"
+    path: path.resolve('dist/'),
+    filename: 'index.js',
   },
   resolve: {
-    extensions: [".mjs", ".js", ".json"]
+    extensions: ['.mjs', '.js', '.json'],
   },
   module: {
     rules: [
       {
         test: /\.m?js$/,
-        loader: "eslint-loader",
-        enforce: "pre",
-        include: [path.resolve("src")],
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [path.resolve('src')],
         options: {
-          formatter: require("eslint-friendly-formatter")
-        }
+          formatter: require('eslint-friendly-formatter'),
+        },
       },
       {
         test: /\.m?js$/,
-        loader: "babel-loader",
-        include: [path.resolve("src")]
-      }
-    ]
+        loader: 'babel-loader',
+        include: [path.resolve('src')],
+      },
+    ],
   },
-  mode: "development",
-  devtool: "eval-source-map",
-  target: "node",
+  mode: 'development',
+  devtool: 'eval-source-map',
+  target: 'node',
   plugins: [
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "src/index.html",
-      inject: true
-    })
-  ]
+      filename: 'index.html',
+      template: 'src/index.html',
+      inject: true,
+    }),
+  ],
 };
