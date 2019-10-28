bf1aa4f3edf42cef0e17666deb46faf076fa14ab
luotao <luotaoyeah@gmail.com>
2019-10-28 17:35:57 +0800

    refactor(javascript-the-definitive-guide): repetition: {n,}

---
+++ src/book/javascript-the-definitive-guide-6e/10/defining-regular-expressions/repetition/03.js
@@ -0,0 +1,10 @@
+(function () {
+  /*
+   * {n,}
+   * 表示模式出现的次数为: n <= 次数
+   */
+
+  const str = '1 22 333 4444';
+
+  console.assert(JSON.stringify(str.match(/\d{2,}/g)) === JSON.stringify(['22', '333', '4444']));
+})();
